86d948b314f9d91ce9c7b5fc0a1307647a176a04
skoulouzis <S.Koulouzis@uva.nl>
2020-09-28 18:29:05 +0200

    1st commit

---
+++ src/main/java/nl/uva/sne/sdia/commons/utils/Converter.java
@@ -0,0 +1,229 @@
+/*
+ * Copyright 2019 S. Koulouzis
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this zipfile except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.uva.sne.sdia.commons.utils;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipOutputStream;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+import nl.uva.sne.sdia.model.tosca.Credential;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.web.multipart.MultipartFile;
+import org.yaml.snakeyaml.Yaml;
+
+/**
+ *
+ * @author S. Koulouzis
+ */
+public class Converter {
+
+    public static String map2YmlString(Map<String, Object> map) throws JSONException {
+        JSONObject jsonObject = new JSONObject(map);
+        String yamlStr = json2Yml2(jsonObject.toString());
+        return yamlStr;
+    }
+
+    public static String json2Yml2(String jsonString) throws JSONException {
+        Yaml yaml = new Yaml();
+        String yamlStr = yaml.dump(ymlString2Map(jsonString));
+        return yamlStr;
+    }
+
+    public static Map<String, Object> ymlString2Map(String yamlString) {
+        Yaml yaml = new Yaml();
+        Object object = yaml.load(yamlString);
+        if (object instanceof List) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("---", object);
+            return map;
+        }
+        return (Map<String, Object>) object;
+    }
+
+    public static String encodeFileToBase64Binary(String fileName) throws IOException {
+        return encode2Base64(Files.readAllBytes(Paths.get(fileName)));
+    }
+
+    public static void decodeBase64BToFile(String base64, String fileName) throws IOException {
+        byte[] decodedBytrs = Base64.getDecoder().decode(base64);
+        Files.write(Paths.get(fileName), decodedBytrs);
+    }
+
+    public static String getFileMD5(String filePath) throws NoSuchAlgorithmException, IOException {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        String keyStoreContents = new String(Files.readAllBytes(Paths.get(filePath)));
+        md.update(keyStoreContents.getBytes());
+        byte[] digest = md.digest();
+        return new String(digest, StandardCharsets.UTF_8);
+    }
+
+    public static String encodeFileToBase64Binary(MultipartFile file) throws IOException {
+
+        String originalFileName = file.getOriginalFilename();
+        String name = System.currentTimeMillis() + "_" + originalFileName;
+        byte[] bytes = file.getBytes();
+
+        return encode2Base64(bytes);
+
+    }
+
+    private static String encode2Base64(byte[] bytes) {
+
+        byte[] encodedBytes = Base64.getEncoder().encode(bytes);
+        return new String(encodedBytes, StandardCharsets.UTF_8);
+    }
+
+    public static void zipFolder(String sourceFolder, String zipFolder) throws FileNotFoundException, IOException {
+        try (FileOutputStream fos = new FileOutputStream(zipFolder);
+                ZipOutputStream zos = new ZipOutputStream(fos)) {
+            Path sourcePath = Paths.get(sourceFolder);
+            Files.walkFileTree(sourcePath, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult preVisitDirectory(final Path dir, final BasicFileAttributes attrs) throws IOException {
+                    if (!sourcePath.equals(dir)) {
+                        zos.putNextEntry(new ZipEntry(sourcePath.relativize(dir).toString() + "/"));
+                        zos.closeEntry();
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(final Path file, final BasicFileAttributes attrs) throws IOException {
+                    zos.putNextEntry(new ZipEntry(sourcePath.relativize(file).toString()));
+                    Files.copy(file, zos);
+                    zos.closeEntry();
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        }
+    }
+
+    public static void unzipFolder(String zipFile, String uncompressedDirectory) throws IOException {
+        try (ZipFile zipfile = new ZipFile(zipFile)) {
+            FileSystem fileSystem = FileSystems.getDefault();
+            Enumeration<? extends ZipEntry> zipEntries = zipfile.entries();
+
+            while (zipEntries.hasMoreElements()) {
+                ZipEntry entry = zipEntries.nextElement();
+                if (entry.isDirectory()) {
+                    Files.createDirectories(fileSystem.getPath(uncompressedDirectory + File.separator + entry.getName()));
+                } else {
+                    InputStream is = zipfile.getInputStream(entry);
+                    BufferedInputStream bis = new BufferedInputStream(is);
+                    String uncompressedFileName = uncompressedDirectory + File.separator + entry.getName();
+                    Path uncompressedFilePath = fileSystem.getPath(uncompressedFileName);
+                    Files.createFile(uncompressedFilePath);
+                    try (FileOutputStream fileOutput = new FileOutputStream(uncompressedFileName)) {
+                        while (bis.available() > 0) {
+                            fileOutput.write(bis.read());
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static String decryptString(String contents, String secret) throws UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        SecretKeySpec secretKey = getsecretKey(secret);
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        return new String(cipher.doFinal(Base64.getDecoder().decode(contents)));
+
+    }
+
+    public static String encryptString(String contents, String secret) throws UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        SecretKeySpec secretKey = getsecretKey(secret);
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        return Base64.getEncoder().encodeToString(cipher.doFinal(contents.getBytes("UTF-8")));
+    }
+
+    private static SecretKeySpec getsecretKey(String myKey) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        MessageDigest sha;
+
+        byte[] key = myKey.getBytes("UTF-8");
+        sha = MessageDigest.getInstance("SHA-1");
+        key = sha.digest(key);
+        key = Arrays.copyOf(key, 16);
+        return new SecretKeySpec(key, "AES");
+    }
+
+    public static Credential encryptCredential(Credential credential, String credentialSecret) throws UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+
+        Map<String, String> credKeys = credential.getKeys();
+        Set<String> keySet = credKeys.keySet();
+        for (String key : keySet) {
+            String credKey = credKeys.get(key);
+            if (credKey != null) {
+                credKeys.put(key, encryptString(credKey, credentialSecret));
+            }
+        }
+        String token = credential.getToken();
+        if (token != null) {
+            credential.setToken(encryptString(token, credentialSecret));
+        }
+        return credential;
+    }
+
+    public static Credential dencryptCredential(Credential credential, String credentialSecret) throws UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Map<String, String> credKeys = credential.getKeys();
+        Set<String> keySet = credKeys.keySet();
+        for (String key : keySet) {
+            String credKey = credKeys.get(key);
+            if (credKey != null) {
+                credKeys.put(key, decryptString(credKey, credentialSecret));
+            }
+        }
+        String token = credential.getToken();
+        if (token != null) {
+            credential.setToken(decryptString(token, credentialSecret));
+        }
+        return credential;
+    }
+
+}
---
+++ src/main/java/nl/uva/sne/sdia/model/Message.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2017 S. Koulouzis.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.uva.sne.sdia.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import nl.uva.sne.sdia.model.tosca.ToscaTemplate;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import java.io.Serializable;
+import java.util.List;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ *
+ *
+ * @author S. Koulouzis.
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Message implements Serializable {
+
+    /**
+     * @return the exception
+     */
+    public Exception getException() {
+        return exception;
+    }
+
+    /**
+     * @param exception the exception to set
+     */
+    public void setException(Exception exception) {
+        this.exception = exception;
+    }
+
+    @NotNull
+    private String owner;
+    @NotNull
+    private Long creationDate;
+
+//    private List<MessageParameter> parameters;
+    private ToscaTemplate toscaTemplate;
+    
+    
+    private Exception exception;
+
+    public Long getCreationDate() {
+        return this.creationDate;
+    }
+
+    public void setCreationDate(Long creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    /**
+     * @return the owner
+     */
+    public String getOwner() {
+        return owner;
+    }
+
+    /**
+     * @param owner the owner to set
+     */
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    /**
+     * @return the toscaTemplate
+     */
+    public ToscaTemplate getToscaTemplate() {
+        return toscaTemplate;
+    }
+
+    /**
+     * @param toscaTemplate the toscaTemplate to set
+     */
+    public void setToscaTemplate(ToscaTemplate toscaTemplate) {
+        this.toscaTemplate = toscaTemplate;
+    }
+
+}
