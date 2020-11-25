50605cebb8ffc24ceffdde63b650e670723c0208
skoulouzis <S.Koulouzis@uva.nl>
2020-11-25 16:36:54 +0100

    ignore null values

--- src/main/java/nl/uva/qcdis/sdia/model/NodeTemplate.java
+++ src/main/java/nl/uva/qcdis/sdia/model/NodeTemplate.java
@@ -2,9 +2,7 @@ package nl.uva.qcdis.sdia.model;
 
 import java.util.Objects;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -12,7 +10,6 @@ import java.util.List;
 import java.util.Map;
 import org.springframework.validation.annotation.Validated;
 import javax.validation.Valid;
-import javax.validation.constraints.*;
 
 /**
  * NodeTemplate
